573a4a09a25058da6f3bf07dc0e3aa385a885a09
聂志良 <1132263176@qq.com>
2018-12-20 15:11:07 +0800

    m

--- src/main/java/com/niezhiliang/signature/utils/controller/IndexController.java
+++ src/main/java/com/niezhiliang/signature/utils/controller/IndexController.java
@@ -39,7 +39,7 @@ public class IndexController {
                 break;
             default:
                 try {
-                    base64 = SealUtils.companyEllipseSeal("浙江某某某网络集团有限公司",0,1,"1234567899876","XX专用");
+                    base64 = SealUtils.companyEllipseSeal("浙江某某某网络集团有限公司",0,0,"1234567899876","XX专用");
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
